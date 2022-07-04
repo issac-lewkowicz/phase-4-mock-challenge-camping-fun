8d47cb7551519df3e81890d609dd820f38ce74d1
Yitzchak Lewkowicz <yitzchaklewkow@yahoo.com>
2022-07-04 11:34:37 -0600

    done

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -1,2 +1,29 @@
 class SignupsController < ApplicationController
+    
+  # def index
+
+  # end
+
+  def create
+    signup = Signup.create!(signup_params)
+    render json: signup.activity, status: :created
+  end
+
+  # def show
+
+  # end
+
+  # def update
+
+  # end
+
+  # def destroy
+
+  # end
+  
+  private
+
+  def signup_params
+    params.permit(:time, :activity_id, :camper_id)
+  end
 end
--- app/models/signup.rb
+++ app/models/signup.rb
@@ -1,4 +1,6 @@
 class Signup < ApplicationRecord
   belongs_to :activity
   belongs_to :camper
+
+  validates :time, presence: true, numericality: { only_integer: true}, inclusion: 0..23 
 end
